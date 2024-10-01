70edca939204803e5d61ca341230d747c176c87a
Mehmet <mehmetgokgul.ybu@gmail.com>
2024-10-01 22:21:54 +0300

    Initial Commit

---
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int count;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    public float speed = 0;
+
+    private float movementX;
+    private float movementY;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+
+        winTextObject.SetActive(false);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 15)
+        {
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+            winTextObject.SetActive(true);
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+            winTextObject.gameObject.SetActive(true);
+            winTextObject.GetComponent<TextMeshProUGUI>().text = "You Lose!";
+        }
+
+    }
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
+        }
+
+    }
+
+}
